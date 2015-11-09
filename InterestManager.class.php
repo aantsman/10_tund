ab8d2094a547cb326f8bffe98ac8d0960b044ab3
Annika Antsman <anniant@greeny.cs.tlu.ee>
2015-11-09 13:28:36 +0200

    interests

--- InterestManager.class.php
+++ InterestManager.class.php
@@ -2,16 +2,63 @@
 class InterestManager {
 	
 	private $connection;
-	private $user_id
+	private $user_id;
 	
 	function __construct($mysqli, $user_id){
 		
 		$this->connection = $mysqli;
 		$this->user_id = $user_id; 
 		
+	}
+	
+	function addInterest($name){
+		
+		//objekt kus tagastame errori (id, message) või successi (message)
+		$response= new StdClass();
+		
+		//kontrollid et sellist huviala veel ei ole
+		$stmt = $this->connection->prepare("SELECT id FROM interests WHERE name=?");
+		$stmt->bind_param("s", $name);
+		$stmt->bind_result($id);
+        $stmt->execute();	
+        
+		//kas saime rea andmeid
+		if($stmt->fetch()){
+			//huviala on juba olemas
+			$error=new StdClass();
+			$error->id = 0;
+			$error->message = "Selline huviala juba olemas!";
+			$response->error=$error;
+			//pärast return käsku ei vaadata funki edasi
+			return $response;
+		}
+		
+		$stmt->close();
+		
+		//kui pole, lisad uue 
+		
+        $stmt = $this->connection->prepare("INSERT INTO interests (name) VALUES (?)");
+        $stmt->bind_param("s", $name);
+		
+        if ($stmt->execute()){
+			//sisestamine õnnestus
+			$success=new StdClass();
+			$success->message= "Huviala edukalt lisatud";
+			$response->success=$success;
+			
+		}
+		else{
+			//ei õnnestunud
+			$error=new StdClass();
+			$error->id = 1;
+			$error->message = "Midagi läks katki.";
+			$response->error=$error;			
+		}
+        $stmt->close();
+		
+		return $response;
 		
 		
 	}
-	
 	
 }?>
--- data.php
+++ data.php
@@ -1,7 +1,9 @@
 <?php
     // kõik mis seotud andmetabeliga, lisamine ja tabeli kujul esitamine
     require_once("functions.php");
-    
+    require_once("InterestManager.class.php");
+	
+	
     //kui kasutaja ei ole sisse logitud, suuna teisele lehele
     //kontrollin kas sessiooni muutuja olemas
     if(!isset($_SESSION['user_id'])){
@@ -18,11 +20,27 @@
 		exit();
     }
 	
+	/***************
+	*****HALDUS*****
+	***************/
 	
+	$InterestManager = new InterestManager(
+	$_SESSION["user_id"]);
 	
+	if(isset($_GET["new_interest"])){
+		
+		$InterestManager->addInterest($_GET["new_interest"]);
+	}
 	
 ?> 
 
 <p>
 Tere, <?=$_SESSION['user_email'];?> <a href="?logout=1">Logi välja</a>
 </p>
+
+<h2> Lisa huviala </h2>
+
+  <form action="data.php" method="post" >
+  	<input name="new_interest"<br>
+  	<input type="submit" name="interest" value="Lisa huviala">
+  </form>
